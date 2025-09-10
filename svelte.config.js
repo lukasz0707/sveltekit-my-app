3a6452cad9071bd1dd3b891e9ea837ae6969d594
lukaszs0707 <lukaszs0707@gmail.com>
2025-09-10 18:12:24 +0200

    github pages

--- svelte.config.js
+++ svelte.config.js
@@ -7,13 +7,17 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
+		appDir: 'app', // Required as the default is _app
 		adapter: adapter({
 			fallback: '404.html'
 		}),
+		// For github pages setup below
 		paths: {
 			// This must match your GitHub repository name exactly.
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-my-app' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
+		// This tells SvelteKit to not prerender any pages by default.
+		// It will only prerender pages with `export const prerender = true`.
 	}
 };
 
